eba9a9e6fda59984b818e8820fc7affb4a593d53
Penano <s6215054@palermo.local>
2018-06-16 11:23:19 -0400

    misc

--- progfun/src/main/scala/week1/session.sc
+++ progfun/src/main/scala/week1/session.sc
@@ -27,6 +27,7 @@ object session {
     if (n==0) 1 else n * factorial_notail(n-1)
 
   def factorial(n: Int): Int = {
+    @annotation.tailrec
     def go(n: Int, acc: Int): Int = {
       if (n==0) acc
       else go(n-1, acc=acc*n)
--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -12,8 +12,15 @@ object Main {
 
   /**
    * Exercise 1
+    * val(c,r) = val(c-1, r-1) + val(c,r-1)
+    * num columns = num rows + 1 (e.g. 4th row (index 3), has 4 columns)
+    * Borders always have coordinates (0,row) or (row+1, row)
    */
-    def pascal(c: Int, r: Int): Int = ???
+
+    def pascal(c: Int, r: Int): Int = {
+      if (c==0 || c==r) 1
+      else pascal(c-1, r-1) + pascal(c, r-1)
+    }
   
   /**
    * Exercise 2
