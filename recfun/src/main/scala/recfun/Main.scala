0e1a307da5189387dbbe182d2d2db600a5b7ebc4
Penano <s6215054@palermo.local>
2018-06-16 20:47:18 -0400

    implement balance parenthesis

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -2,12 +2,21 @@ package recfun
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
+//    println("Pascal's Triangle")
+//    for (row <- 0 to 10) {
+//      for (col <- 0 to row)
+//        print(pascal(col, row) + " ")
+//      println()
+//    }
+
+//    println(balance("(if (zero? x) max (/ 1 x))".toList)) // true
+//    println(balance("".toList))  // true
+//    println(balance("())".toList))  // false
+//    println(balance("((())".toList))  // false
+//    println(balance("(if (zero? x) max (/ 1 x)))".toList)) // false
+//    println(balance("(if (zero? x) max (/ 1 x)()".toList)) // false
+//    println(balance("())(".toList))  // false
+
   }
 
   /**
@@ -24,8 +33,27 @@ object Main {
   
   /**
    * Exercise 2
+    * If at the end num. of left bracket != num. right bracket
+    * and list is empty, false
+    * If number of left bracket < right bracket, false immediately
+    * if list is empty from start, true
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      @annotation.tailrec
+      def go(chars: List[Char], rbc: Int, lbc: Int): Boolean = {
+        if (chars.isEmpty && lbc != rbc) false
+        else if (chars.isEmpty) true
+        else if (lbc < rbc) false
+        else {
+          if (chars.head == '(') go(chars.tail, rbc=rbc, lbc=lbc+1)
+          else if (chars.head == ')') go(chars.tail, rbc=rbc+1, lbc=lbc)
+          else go(chars.tail, rbc=rbc, lbc=lbc)
+        }
+      }
+
+      go(chars, rbc=0, lbc=0)
+
+    }
   
   /**
    * Exercise 3
